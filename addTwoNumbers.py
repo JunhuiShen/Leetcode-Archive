fd8cf21ce31776c2b95ecc476f34e554bacd3021
Junhui Shen <50338971+JunhuiShen@users.noreply.github.com>
2024-05-24 13:48:26 -0700

    Update addTwoNumbers.py

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,3 +1,17 @@
+# Example 1:
+
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+# Example 2:
+
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+# Example 3:
+
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
@@ -32,6 +46,3 @@ class Solution(object):
             current.next = ListNode(carry)
         
         return dummy_head.next
-        
-
-        
